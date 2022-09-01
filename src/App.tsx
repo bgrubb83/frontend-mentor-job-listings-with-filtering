9f89f4dff3f48ec5a5cf0ae5268aded8ba5266e4
Barry Grubb <bgrubb83@gmail.com>
2022-09-01 21:26:28 +0100

    Remove unused CRA files, fetch data.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import rawData from './dataStore/data.json';
 
 function App() {
+  console.log(rawData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <p>App</p>
+    {rawData.map((data) => {
+      return <p key={data.id}>{data.id}</p>
+    })}
+    </>
   );
 }
 
