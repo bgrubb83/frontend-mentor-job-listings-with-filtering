883b5cb57dc0edf7f0fbbc6d73ecf11a5f0da817
Barry Grubb <bgrubb83@gmail.com>
2022-09-14 21:22:28 +0100

    Add filtering by tag (no ux).

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import rawData from './dataStore/data.json';
 
 import JobCard from './components/JobCard';
-import { JobCardData, JobData } from './components/JobCard/JobCard.d'
+import { JobCardData } from './components/JobCard/JobCard.d'
 
 import processData from './helpers/processData';
 
-const handleAddTag = (tag: string): void => {
-  console.log(tag)
-}
 
 function App() {
-  console.log(rawData)
-  const processedData = processData(rawData);
+  const [tags, setTags] = useState<string[]>([]);
+
+  const handleAddTag = (tag: string): void => {
+    if (!tags.includes(tag)) {
+      const updatedTags: string[] = [...tags];
+      updatedTags.push(tag);
+      setTags(updatedTags);
+    }
+  }
+  
+  let processedData = processData(rawData);
+
+  if (tags && tags.length) {
+    processedData = processedData.filter((job: JobCardData) => {
+      let tagged: boolean = true;
+      tags.forEach((tag: string) => {
+        if (!job.tags?.includes(tag)) {
+          tagged = false;
+        }
+      })
+      return tagged;
+    })
+  }
 
   return (
     <div className='jobsListWrapper'>
-    {processedData.map((job: JobCardData) => {
-      return <JobCard job={job} handleAddTag={handleAddTag} key={job.id} />
-    })}
+      {processedData.map((job: JobCardData) => {
+        return <JobCard job={job} handleAddTag={handleAddTag} key={job.id} />
+      })}
     </div>
   );
 }
