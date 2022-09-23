fd9acafaddaa9df8210f77ad51c36a517080cd8c
Barry Grubb <bgrubb83@gmail.com>
2022-09-23 20:36:51 +0100

    Styling on ActiveTagCard.

--- src/components/ActiveTagCard/ActiveTagCard.tsx
+++ src/components/ActiveTagCard/ActiveTagCard.tsx
@@ -9,10 +9,12 @@ import Link from '../Link';
 const ActiverTagCard: React.FC<ActiveTagCardData> = ({ tags, handleClearTags, handleRemoveTag }) => {
     return (
         <section className='active-tag-card-wrapper'>
-            {tags.map((tag) => {
-                return <ActiveTag key={tag} tag={tag} onClick={handleRemoveTag}/>
-            })}
-            <Link label='clear' onClick={handleClearTags} />
+            <section className='inner-tag-wrapper'>
+                {tags.map((tag) => {
+                    return <ActiveTag key={tag} tag={tag} onClick={handleRemoveTag} />
+                })}
+            </section>
+            <Link label='Clear' onClick={handleClearTags} />
         </section>
     )
 }
--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -6,7 +6,7 @@ import './Link.css';
 const Link: React.FC<LinkData> = ({ label, onClick }) => {
     return (
         <span onClick={onClick} >
-            <p>{label}</p>
+            <p className='link-text'>{label}</p>
         </span>
     )
 }
